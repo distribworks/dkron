05671b62786dcb6626b5b4944182ffc6b4ecec41
Victor Castell <victor@victorcastell.com>
2015-05-21 22:14:04 +0200

    Refactor some methods extracted to it's source file
    Also moved event listening logic to server

--- dcron/serf.go
+++ dcron/serf.go
@@ -1,14 +1,12 @@
 package dcron
 
 import (
-	"fmt"
-	"github.com/hashicorp/serf/client"
-	serfs "github.com/hashicorp/serf/serf"
-	"os"
 	"os/exec"
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/hashicorp/serf/client"
 )
 
 type serfManager struct {
@@ -16,49 +14,10 @@ type serfManager struct {
 	Agent *exec.Cmd
 }
 
-var serf *serfManager
+var serf = &serfManager{}
 
-func (sm *serfManager) Terminate() {
-	serf.Close()
-	sm.Agent.Process.Signal(syscall.SIGKILL)
-}
-
-func NewSerfManager(agent *exec.Cmd) *serfManager {
-	serfConfig := &client.Config{Addr: config.GetString("rpc_addr")}
-	sc, err := client.ClientFromConfig(serfConfig)
-	// wait for serf
-	for i := 0; err != nil && i < 5; i = i + 1 {
-		log.Debug(err)
-		time.Sleep(1 * time.Second)
-		sc, err = client.ClientFromConfig(serfConfig)
-		log.Debugf("Connect to serf agent retry: %d", i)
-	}
-	if err != nil {
-		log.Error("Error connecting to serf instance", err)
-		return nil
-	}
-	return &serfManager{sc, agent}
-}
-
-// spawn command that specified as proc.
-func spawnProc(proc string) (*exec.Cmd, error) {
-	cs := []string{"/bin/bash", "-c", proc}
-	cmd := exec.Command(cs[0], cs[1:]...)
-	cmd.Stdin = nil
-	cmd.Stdout = log.Writer()
-	cmd.Stderr = log.Writer()
-	cmd.Env = append(os.Environ())
-
-	fmt.Fprintf(log.Writer(), "Starting %s\n", proc)
-	err := cmd.Start()
-	if err != nil {
-		fmt.Fprintf(log.Writer(), "Failed to start %s: %s\n", proc, err)
-		return nil, err
-	}
-	return cmd, nil
-}
-
-func init() {
+// It start the local serf agent waits until it's started to connect the RPC client.
+func (sm *serfManager) Start() {
 	discover := ""
 	if config.GetString("discover") != "" {
 		discover = " -discover=" + config.GetString("discover")
@@ -69,35 +28,27 @@ func init() {
 		log.Error(err)
 	}
 
-	serf = NewSerfManager(agent)
+	sm.Agent = agent
 
-	go func() {
-		ch := make(chan map[string]interface{}, 1)
+	serfConfig := &client.Config{Addr: config.GetString("rpc_addr")}
+	sc, err := client.ClientFromConfig(serfConfig)
+	// wait for serf
+	for i := 0; err != nil && i < 5; i = i + 1 {
+		log.Debug(err)
+		time.Sleep(1 * time.Second)
+		sc, err = client.ClientFromConfig(serfConfig)
+		log.Debugf("Connect to serf agent retry: %d", i)
+	}
+	if err != nil {
+		log.Fatal("Error connecting to serf instance", err)
+	}
 
-		sh, err := serf.Stream("*", ch)
-		if err != nil {
-			log.Error(err)
-		}
-		defer serf.Stop(sh)
+	sm.RPCClient = sc
+}
 
-		for {
-			select {
-			case event := <-ch:
-				for key, val := range event {
-					switch ev := val.(type) {
-					case serfs.MemberEvent:
-						log.Debug(ev)
-					default:
-						log.Debugf("Receiving event: %s => %v of type %T", key, val, val)
-					}
-				}
-				if event["Event"] == "query" {
-					log.Debug(string(event["Payload"].([]byte)))
-					serf.Respond(uint64(event["ID"].(int64)), []byte("Peetttee"))
-				}
-			}
-		}
-	}()
+func (sm *serfManager) Terminate() {
+	sm.Close()
+	sm.Agent.Process.Signal(syscall.SIGKILL)
 }
 
 type Event struct {
