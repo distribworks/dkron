8b1d88daf4275c51a78a2f35c56ea91022a891ca
Victor Castell <victor@victorcastell.com>
2015-06-08 22:21:42 +0200

    More config paths and gracefully fallback to config defaults

--- dcron/config.go
+++ dcron/config.go
@@ -35,11 +35,13 @@ type Config struct {
 const DefaultBindPort int = 7946
 
 func init() {
-	viper.SetConfigName("dcron")    // name of config file (without extension)
-	viper.AddConfigPath("./config") // call multiple times to add many search paths
-	err := viper.ReadInConfig()     // Find and read the config file
-	if err != nil {                 // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	viper.SetConfigName("dcron")        // name of config file (without extension)
+	viper.AddConfigPath("/etc/dcron")   // call multiple times to add many search paths
+	viper.AddConfigPath("$HOME/.dcron") // call multiple times to add many search paths
+	viper.AddConfigPath("./config")     // call multiple times to add many search paths
+	err := viper.ReadInConfig()         // Find and read the config file
+	if err != nil {                     // Handle errors reading the config file
+		log.Errorf("No valid config found: %s \n Applying default values.", err)
 	}
 
 	viper.SetEnvPrefix("dcr") // will be uppercased automatically
