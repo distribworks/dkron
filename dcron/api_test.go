0d341c734c7dcc164cc26c10063fe7a234f97597
Victor Castell <victor@victorcastell.com>
2015-06-22 22:49:50 +0200

    Testing the API

--- dcron/api_test.go
+++ dcron/api_test.go
@@ -1,1 +1,58 @@
 package dcron
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/mitchellh/cli"
+)
+
+func TestApiJobReschedule(t *testing.T) {
+	log.Level = logrus.DebugLevel
+
+	shutdownCh := make(chan struct{})
+	defer close(shutdownCh)
+
+	ui := new(cli.MockUi)
+	a := &AgentCommand{
+		Ui:         ui,
+		ShutdownCh: shutdownCh,
+	}
+
+	args := []string{
+		"-bind", "127.0.0.1:8970",
+	}
+
+	resultCh := make(chan int)
+	go func() {
+		resultCh <- a.Run(args)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
+
+	var jsonStr = []byte(`{"name": "test_job", "schedule": "@every 2s", "command": "date", "owner": "mec", "owner_email": "foo@bar.com"}`)
+	resp, err := http.Post("http://localhost:8970/jobs/", "encoding/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	t.Log(body)
+
+	// Send a shutdown request
+	shutdownCh <- struct{}{}
+
+	select {
+	case code := <-resultCh:
+		if code != 0 {
+			t.Fatalf("bad code: %d", code)
+		}
+	case <-time.After(50 * time.Millisecond):
+		t.Fatalf("timeout")
+	}
+}
