2179ee0894953c1bd46334a8bf11ab03e7c60f54
Victor Castell <victor@victorcastell.com>
2015-07-10 00:18:04 +0200

    Failed series

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -21,16 +21,20 @@ dcron.controller('IndexCtrl', function ($scope, $http, $interval, $element) {
 
   $scope.series = [{
       name: 'Success count',
-      color: 'steelblue',
+      color: 'darkgreen',
+      data: [{x: 0, y: 0}]
+  },{
+      name: 'Error count',
+      color: 'red',
       data: [{x: 0, y: 0}]
   }];
   $scope.features = {
       hover: {
           xFormatter: function(x) {
-              return 't=' + x;
+              return x;
           },
           yFormatter: function(y) {
-              return '$' + y;
+              return y;
           }
       },
       legend: {
@@ -58,6 +62,7 @@ dcron.controller('IndexCtrl', function ($scope, $http, $interval, $element) {
   }, 2000);
 
   $scope.success_count = 0;
+  $scope.error_count = 0;
 
   $scope.updateGraph = function(data) {
     var gdata = $scope.series[0].data;
@@ -77,6 +82,27 @@ dcron.controller('IndexCtrl', function ($scope, $http, $interval, $element) {
     gdata.push({x: gdata[gdata.length - 1].x + 1, y: diff})
 
     $scope.series[0] = {
+      name: name,
+      color: color,
+      data: gdata
+    };
+
+    gdata = $scope.series[1].data;
+    name = $scope.series[1].name;
+    color = $scope.series[1].color;
+    var error_count = 0;
+
+    for(i=0; i < data.length; i++) {
+      error_count = error_count + data[i].error_count;
+    }
+    if($scope.error_count != 0) {
+      diff = error_count - $scope.error_count;
+    }
+    $scope.error_count = error_count;
+
+    gdata.push({x: gdata[gdata.length - 1].x + 1, y: diff})
+
+    $scope.series[1] = {
       name: name,
       color: color,
       data: gdata
