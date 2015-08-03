fc4ca4a4347e47e3ab0710493810663a72ca541c
Victor Castell <victor@victorcastell.com>
2015-08-03 21:29:52 +0100

    Deployed 7627c80 with MkDocs version: 0.14.0

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
-  $(document).scroll(function(){
-    if($(window).scrollTop() <= '0'){
-      $('.navbar-default').addClass('nav-top');
-    } else {
-      $('.navbar-default').removeClass('nav-top');
-    }
 
-  });
 
 });
