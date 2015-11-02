b2ca9afdcb43c1ba35f8b30340323c3b70466bde
Victor Castell <victor@victorcastell.com>
2015-11-02 18:29:58 +0100

    Weird behaviour in Travis
    For some strange reason doing a watch running the tests in Travis, justs
    listents for the first event and doesn't listen to the following events.

--- dkron/agent_test.go
+++ dkron/agent_test.go
@@ -141,7 +141,6 @@ func TestAgentCommandElectLeader(t *testing.T) {
 	for {
 		select {
 		case res := <-receiver:
-			println(string(res.Value))
 			if bytes.Equal(res.Value, []byte(test2Key)) {
 				t.Logf("Leader changed: %s", res.Value)
 				return
