0bed84272e251b6916521341a5004718b13e1b14
Victor Castell <victor@victorcastell.com>
2015-12-23 15:59:36 +0100

    Utils

---
+++ dkron/utils.go
@@ -0,0 +1,21 @@
+package dkron
+
+import (
+	"strings"
+	"unicode"
+)
+
+// For example: generateSlug("My cool object") returns "my-cool-object"
+func generateSlug(str string) (slug string) {
+	return strings.Map(func(r rune) rune {
+		switch {
+		case r == ' ', r == '-':
+			return '-'
+		case r == '_', unicode.IsLetter(r), unicode.IsDigit(r):
+			return r
+		default:
+			return -1
+		}
+		return -1
+	}, strings.ToLower(strings.TrimSpace(str)))
+}
