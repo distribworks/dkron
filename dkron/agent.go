2ac336de3f8fab1ed14c1ef84f80022ab2b83847
Victor Castell <victor@victorcastell.com>
2016-06-10 01:08:40 +0200

    Reload config

--- dkron/agent.go
+++ dkron/agent.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"expvar"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -21,7 +20,6 @@ import (
 	"github.com/hashicorp/memberlist"
 	"github.com/hashicorp/serf/serf"
 	"github.com/mitchellh/cli"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -98,120 +96,6 @@ Options:
   -log-level=info                 Log level (debug, info, warn, error, fatal, panic). Default to info.
 `
 	return strings.TrimSpace(helpText)
-}
-
-// readConfig is responsible for setup of our configuration using
-// the command line and any file configs
-func (a *AgentCommand) readConfig(args []string) *Config {
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	cmdFlags := flag.NewFlagSet("server", flag.ContinueOnError)
-	cmdFlags.Usage = func() { a.Ui.Output(a.Help()) }
-	cmdFlags.String("node", hostname, "node name")
-	viper.SetDefault("node_name", cmdFlags.Lookup("node").Value)
-	cmdFlags.String("bind", fmt.Sprintf("0.0.0.0:%d", DefaultBindPort), "address to bind listeners to")
-	viper.SetDefault("bind_addr", cmdFlags.Lookup("bind").Value)
-	cmdFlags.String("advertise", "", "address to advertise to other nodes")
-	viper.SetDefault("advertise_addr", cmdFlags.Lookup("advertise").Value)
-	cmdFlags.String("http-addr", ":8080", "HTTP address")
-	viper.SetDefault("http_addr", cmdFlags.Lookup("http-addr").Value)
-	cmdFlags.String("discover", "dkron", "mDNS discovery name")
-	viper.SetDefault("discover", cmdFlags.Lookup("discover").Value)
-	cmdFlags.String("backend", "etcd", "store backend")
-	viper.SetDefault("backend", cmdFlags.Lookup("backend").Value)
-	cmdFlags.String("backend-machine", "127.0.0.1:2379", "store backend machines addresses")
-	viper.SetDefault("backend_machine", cmdFlags.Lookup("backend-machine").Value)
-	cmdFlags.String("profile", "lan", "timing profile to use (lan, wan, local)")
-	viper.SetDefault("profile", cmdFlags.Lookup("profile").Value)
-	viper.SetDefault("server", cmdFlags.Bool("server", false, "start dkron server"))
-	var startJoin []string
-	cmdFlags.Var((*AppendSliceValue)(&startJoin), "join", "address of agent to join on startup")
-	var tag []string
-	cmdFlags.Var((*AppendSliceValue)(&tag), "tag", "tag pair, specified as key=value")
-	cmdFlags.String("keyspace", "dkron", "key namespace to use")
-	viper.SetDefault("keyspace", cmdFlags.Lookup("keyspace").Value)
-	cmdFlags.String("encrypt", "", "encryption key")
-	viper.SetDefault("encrypt", cmdFlags.Lookup("encrypt").Value)
-
-	cmdFlags.String("log-level", "info", "Log level (debug, info, warn, error, fatal, panic), defaults to info")
-	viper.SetDefault("log_level", cmdFlags.Lookup("log-level").Value)
-
-	cmdFlags.String("ui-dir", ".", "directory to serve web UI")
-	viper.SetDefault("ui_dir", cmdFlags.Lookup("ui-dir").Value)
-	viper.SetDefault("rpc_port", cmdFlags.Int("rpc-port", 6868, "RPC port"))
-
-	// Notifications
-	cmdFlags.String("mail-host", "", "notification mail server host")
-	viper.SetDefault("mail_host", cmdFlags.Lookup("mail-host").Value)
-	cmdFlags.String("mail-port", "", "port to use for the mail server")
-	viper.SetDefault("mail_port", cmdFlags.Lookup("mail-port").Value)
-	cmdFlags.String("mail-username", "", "username for the mail server")
-	viper.SetDefault("mail_username", cmdFlags.Lookup("mail-username").Value)
-	cmdFlags.String("mail-password", "", "password of the mail server")
-	viper.SetDefault("mail_password", cmdFlags.Lookup("mail-password").Value)
-	cmdFlags.String("mail-from", "", "notification emails from address")
-	viper.SetDefault("mail_from", cmdFlags.Lookup("mail-from").Value)
-
-	cmdFlags.String("webhook-url", "", "notification webhook url")
-	viper.SetDefault("webhook_url", cmdFlags.Lookup("webhook-url").Value)
-	cmdFlags.String("webhook-payload", "", "notification webhook payload")
-	viper.SetDefault("webhook_payload", cmdFlags.Lookup("webhook-payload").Value)
-	webhookHeaders := &AppendSliceValue{}
-	cmdFlags.Var(webhookHeaders, "webhook-header", "notification webhook additional header")
-
-	if err := cmdFlags.Parse(args); err != nil {
-		log.Fatal(err)
-	}
-
-	ut, err := UnmarshalTags(tag)
-	if err != nil {
-		log.Fatal(err)
-	}
-	viper.SetDefault("tags", ut)
-	viper.SetDefault("join", startJoin)
-	viper.SetDefault("webhook_headers", webhookHeaders)
-
-	tags := viper.GetStringMapString("tags")
-	server := viper.GetBool("server")
-	nodeName := viper.GetString("node_name")
-
-	if server {
-		tags["dkron_server"] = "true"
-	}
-	tags["dkron_version"] = a.Version
-
-	InitLogger(viper.GetString("log_level"), nodeName)
-
-	return &Config{
-		NodeName:        nodeName,
-		BindAddr:        viper.GetString("bind_addr"),
-		AdvertiseAddr:   viper.GetString("advertise_addr"),
-		HTTPAddr:        viper.GetString("http_addr"),
-		Discover:        viper.GetString("discover"),
-		Backend:         viper.GetString("backend"),
-		BackendMachines: viper.GetStringSlice("backend_machine"),
-		Server:          server,
-		Profile:         viper.GetString("profile"),
-		StartJoin:       viper.GetStringSlice("join"),
-		Tags:            tags,
-		Keyspace:        viper.GetString("keyspace"),
-		EncryptKey:      viper.GetString("encrypt"),
-		UIDir:           viper.GetString("ui_dir"),
-		RPCPort:         viper.GetInt("rpc_port"),
-
-		MailHost:     viper.GetString("mail_host"),
-		MailPort:     uint16(viper.GetInt("mail_port")),
-		MailUsername: viper.GetString("mail_username"),
-		MailPassword: viper.GetString("mail_password"),
-		MailFrom:     viper.GetString("mail_from"),
-
-		WebhookURL:     viper.GetString("webhook_url"),
-		WebhookPayload: viper.GetString("webhook_payload"),
-		WebhookHeaders: viper.GetStringSlice("webhook_headers"),
-	}
 }
 
 // setupSerf is used to create the agent we use
@@ -394,7 +278,7 @@ func UnmarshalTags(tags []string) (map[string]string, error) {
 }
 
 func (a *AgentCommand) Run(args []string) int {
-	a.config = a.readConfig(args)
+	a.config = NewConfig(args, a)
 	if a.serf = a.setupSerf(); a.serf == nil {
 		log.Fatal("agent: Can not setup serf")
 	}
@@ -421,6 +305,7 @@ func (a *AgentCommand) handleSignals() int {
 	signalCh := make(chan os.Signal, 4)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
 
+WAIT:
 	// Wait for a signal
 	var sig os.Signal
 	select {
@@ -430,6 +315,12 @@ func (a *AgentCommand) handleSignals() int {
 		sig = os.Interrupt
 	}
 	a.Ui.Output(fmt.Sprintf("Caught signal: %v", sig))
+
+	// Check if this is a SIGHUP
+	if sig == syscall.SIGHUP {
+		a.handleReload()
+		goto WAIT
+	}
 
 	// Check if we should do a graceful leave
 	graceful := false
@@ -469,6 +360,22 @@ func (a *AgentCommand) handleSignals() int {
 	case <-gracefulCh:
 		return 0
 	}
+}
+
+// handleReload is invoked when we should reload our configs, e.g. SIGHUP
+func (a *AgentCommand) handleReload() {
+	a.Ui.Output("Reloading configuration...")
+	newConf := ReadConfig(a)
+	if newConf == nil {
+		a.Ui.Error(fmt.Sprintf("Failed to reload configs"))
+		return
+	} else {
+		a.config = newConf
+	}
+
+	// Reset serf tags
+	a.serf.SetTags(a.config.Tags)
+	//Config reloading will also reload Notification settings
 }
 
 func (a *AgentCommand) Synopsis() string {
