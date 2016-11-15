1efa1dc4299c77cf87bb961ae226d108e3789ed6
Victor Castell <victor@victorcastell.com>
2016-11-15 22:35:57 +0100

    Code comments

--- builtin/bins/dkron-processor-files/files_output.go
+++ builtin/bins/dkron-processor-files/files_output.go
@@ -11,11 +11,14 @@ import (
 
 const defaultLogDir = "/var/log/dkron"
 
+// Files output plugin that saves each execution log
+// in it's own file in the file system.
 type FilesOutput struct {
 	forward bool
 	logDir  string
 }
 
+// Plugin process
 func (l *FilesOutput) Process(args *dkron.ExecutionProcessorArgs) dkron.Execution {
 	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
 	l.parseConfig(args.Config)
