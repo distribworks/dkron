a10e1904ea67c1ba51576018cb61a8e52ec77857
Victor Castell <victor@victorcastell.com>
2016-12-03 23:18:23 +0100

    Test with docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose up -d etcd
-export COMPOSE_ETCD_PORT=`docker port dkron_etcd_1 4001/tcp | cut -d":" -f 2`
-export DKRON_BACKEND_MACHINE=`docker-machine ip default`:$COMPOSE_ETCD_PORT
-go test -v $(glide novendor) $1
+docker-compose run -e DKRON_BACKEND_MACHINE=etcd:4001 dkron go test -v $(glide novendor) $1
