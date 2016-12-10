82e7be09880c29bd56cc3edf7b2620a180157e7c
Victor Castell <victor@victorcastell.com>
2016-12-10 23:39:52 +0100

    Fix glide novendor docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 docker-compose run dkron scripts/validate-gofmt
 docker-compose run dkron go vet ./...
-docker-compose run -e DKRON_BACKEND_MACHINE=etcd:4001 dkron go test -v $(docker-compose run dkron glide novendor) $1
+docker-compose run -e DKRON_BACKEND_MACHINE=etcd:4001 dkron go test -v $(docker-compose run dkron glide novendor | tr -d '\r') $1
