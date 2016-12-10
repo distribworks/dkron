a3077a2c55db6fc6d059c5d928e2c3786ce7dcaf
Victor Castell <victor@victorcastell.com>
2016-12-10 23:11:59 +0100

    Fully test with docker in travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+docker-compose run dkron scripts/validate-gofmt
+docker-compose run dkron go vet ./...
 docker-compose run -e DKRON_BACKEND_MACHINE=etcd:4001 dkron go test -v $(glide novendor) $1
