d36f4959e7e355e6268abfd73cd5aeefffb413e9
Victor Castell <victor@victorcastell.com>
2016-12-11 23:21:04 +0100

    Reorg testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose run dkron glide install
-docker-compose run dkron scripts/validate-gofmt
 NOVENDOR=$(docker-compose run dkron glide novendor | tr -d '\r')
+
+docker-compose run dkron scripts/validate-gofmt
 docker-compose run dkron go vet $NOVENDOR
 docker-compose run -e DKRON_BACKEND_MACHINE=etcd:4001 dkron go test -v $NOVENDOR $1
