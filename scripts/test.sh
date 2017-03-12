b9ed6b360259a834314b2257310bdc3300d2d81f
Victor Castell <victor@victorcastell.com>
2017-03-12 23:53:21 +0100

    Fix testing backend

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ NOVENDOR=$(docker-compose run dkron glide novendor | tr -d '\r')
 
 docker-compose run dkron scripts/validate-gofmt
 docker-compose run dkron go vet $NOVENDOR
-docker-compose run -e DKRON_BACKEND_MACHINE=etcd:2389 dkron go test -v $NOVENDOR $1
+docker-compose run -e DKRON_BACKEND_MACHINE=etcd:2379 dkron go test -v $NOVENDOR $1
