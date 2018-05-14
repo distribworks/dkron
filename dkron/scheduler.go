8519724ef7c0520ce3510c7a2684cf8ad34a53f8
Victor Castell <victor@victorcastell.com>
2018-05-14 22:21:50 +0200

    Fix type

--- dkron/scheduler.go
+++ dkron/scheduler.go
@@ -23,7 +23,7 @@ type Cron interface {
 	Entries() []*cron.Entry
 	AddFunc(spec string, cmd func()) error
 	AddJob(spec string, cmd cron.Job) error
-	AddTimezoneSensitiveJob(spec, timezone string, cmd Job) error
+	AddTimezoneSensitiveJob(spec, timezone string, cmd cron.Job) error
 }
 
 type Scheduler struct {
