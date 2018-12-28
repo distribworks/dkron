c9c692c7ea0a5d7b59d9b568677d71597dffa90f
Victor Castell <victor@victorcastell.com>
2018-12-28 18:23:47 +0100

    Remove unmarshalTags from dkron.go

--- cmd/dkron.go
+++ cmd/dkron.go
@@ -83,17 +83,3 @@ func initConfig() {
 
 	dkron.InitLogger(viper.GetString("log-level"), config.NodeName)
 }
-
-// unmarshalTags is a utility function which takes a slice of strings in
-// key=value format and returns them as a tag mapping.
-func unmarshalTags(tags []string) (map[string]string, error) {
-	result := make(map[string]string)
-	for _, tag := range tags {
-		parts := strings.SplitN(tag, "=", 2)
-		if len(parts) != 2 || len(parts[0]) == 0 {
-			return nil, fmt.Errorf("Invalid tag: '%s'", tag)
-		}
-		result[parts[0]] = parts[1]
-	}
-	return result, nil
-}
