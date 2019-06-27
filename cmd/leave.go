5f261bb2ae42d675b589eb8ab77a85ba1bc11d16
Victor Castell <victor@victorcastell.com>
2019-06-27 23:24:12 +0200

    Leave info

--- cmd/leave.go
+++ cmd/leave.go
@@ -13,10 +13,7 @@ var leaveCmd = &cobra.Command{
 	Short: "Force an agent to leave the cluster",
 	Long: `Stop stops an agent, if the agent is a server and is running for election
 	stop running for election, if this server was the leader
-	this will force the cluster to elect a new leader and start a new scheduler.
-	If this is a server and has the scheduler started stop it, ignoring if this server
-	was participating in leader election or not (local storage).
-	Then actually leave the cluster.`,
+	this will force the cluster to elect a new leader and start a new scheduler.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var gc dkron.DkronGRPCClient
 		gc = dkron.NewGRPCClient(nil, nil)
