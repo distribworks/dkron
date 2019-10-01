1d7ca6ee69a6990fe1c211be36bbcd9dd6204dbb
Yuri van Oers <yvanoers@gmail.com>
2019-10-01 23:32:41 +0200

    Make SimpleSchedule never run after schedule has run, instead of 10 years from now

--- extcron/simple.go
+++ extcron/simple.go
@@ -4,12 +4,12 @@ import (
 	"time"
 )
 
-// SimpleDelaySchedule represents a simple non recurring duration.
+// SimpleSchedule represents a simple non recurring duration.
 type SimpleSchedule struct {
 	Date time.Time
 }
 
-// Just store the given time for this schedule.
+// At just stores the given time for this schedule.
 func At(date time.Time) SimpleSchedule {
 	return SimpleSchedule{
 		Date: date,
@@ -19,11 +19,11 @@ func At(date time.Time) SimpleSchedule {
 // Next conforms to the Schedule interface but this kind of jobs
 // doesn't need to be run more than once, so it doesn't return a new date but the existing one.
 func (schedule SimpleSchedule) Next(t time.Time) time.Time {
-	// If the date set is after the reference time return it
-	// if it's before, return a virtually infinite sleep date
-	// so do nothing.
+	// If the date set is after the reference time return it.
+	// If it's before, return a time in the past (01-01-0001)
+	// so it never runs.
 	if schedule.Date.After(t) {
 		return schedule.Date
 	}
-	return t.AddDate(10, 0, 0)
+	return time.Time{}
 }
