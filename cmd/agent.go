1be41b35b133088472410d254be546adeea9fa56
Andrey Golev <a.golevs@dynatech.lv>
2020-03-05 12:29:36 +0200

    Graceful shutdown. Exiting cluster, then waiting for all ongoing jobs to finish before dying.

--- cmd/agent.go
+++ cmd/agent.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/distribworks/dkron/v2/dkron"
-	"github.com/hashicorp/go-plugin"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -20,7 +19,7 @@ var agent *dkron.Agent
 
 const (
 	// gracefulTimeout controls how long we wait before forcefully terminating
-	gracefulTimeout = 3 * time.Second
+	gracefulTimeout = 3 * time.Hour
 )
 
 // agentCmd represents the agent command
@@ -110,17 +109,27 @@ WAIT:
 	}
 
 	// Attempt a graceful leave
-	gracefulCh := make(chan struct{})
 	log.Info("agent: Gracefully shutting down agent...")
 	go func() {
-		plugin.CleanupClients()
 		if err := agent.Stop(); err != nil {
 			fmt.Printf("Error: %s", err)
 			log.Error(fmt.Sprintf("Error: %s", err))
 			return
 		}
-		close(gracefulCh)
 	}()
+
+	gracefulCh := make(chan struct{})
+
+	for {
+		log.Info("Waiting for jobs to finish...")
+		if agent.GetRunningJobs() < 1 {
+			log.Info("No jobs left. Exiting.")
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	close(gracefulCh)
 
 	// Wait for leave or another signal
 	select {
