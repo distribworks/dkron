6eb3ca871058f1e4a57b784b58811dac8405f2bd
Victor Castell <victor@victorcastell.com>
2021-01-24 23:30:02 +0100

    feat: Show execution status

--- ui/src/jobs/StatusFiled.tsx
+++ ui/src/jobs/StatusFiled.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import SuccessIcon from '@material-ui/icons/CheckCircle';
 import FailedIcon from '@material-ui/icons/Cancel';
+import { Tooltip } from '@material-ui/core';
 
 const StatusField = (props: any) => {
-  return (props.record[props.source] === 'success' ? <SuccessIcon htmlColor="green" /> : <FailedIcon htmlColor="red" />);
+  return (props.record[props.source] === 'success' ? <Tooltip title="Success"><SuccessIcon htmlColor="green" /></Tooltip> : <Tooltip title="Success"><FailedIcon htmlColor="red" /></Tooltip>);
 };
 
 export default StatusField;
