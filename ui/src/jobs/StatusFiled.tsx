03f325f036468785c03707bba226bbbd3b1705d3
Miguel Sousa <miguelsousa@MacBook-Pro-de-Miguel.local>
2021-03-21 15:34:45 +0000

    FIX conflicts status

--- ui/src/jobs/StatusFiled.tsx
+++ ui/src/jobs/StatusFiled.tsx
@@ -5,12 +5,16 @@ import PristineIcon from '@material-ui/icons/Timer';
 import { Tooltip } from '@material-ui/core';
 
 const StatusField = (props: any) => {
-	if (props.record[props.source] === 'success') {
-		return <Tooltip title="Success"><SuccessIcon htmlColor="green" /></Tooltip> 
-	} else if (props.record[props.source] === 'failed') {
-		return <Tooltip title="Error"><FailedIcon htmlColor="red" /></Tooltip>
+	if (props.record === undefined) {
+		return null
 	} else {
-		return <Tooltip title="Waiting to Run"><PristineIcon htmlColor="blue" /></Tooltip>
+		if (props.record[props.source] === 'success') {
+			return <Tooltip title="Success"><SuccessIcon htmlColor="green" /></Tooltip> 
+		} else if (props.record[props.source] === 'failed') {
+			return <Tooltip title="Error"><FailedIcon htmlColor="red" /></Tooltip>
+		} else {
+			return <Tooltip title="Waiting to Run"><PristineIcon htmlColor="blue" /></Tooltip>
+		}
 	}
 };
 
