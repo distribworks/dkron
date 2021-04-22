5c69807cdfe4aad97c1a7108b2b77ec2d25bb1ec
Moshe Beladev <moshebe123@gmail.com>
2021-04-22 12:08:47 +0300

    add gcp pubsub executor to bulitin plugins

---
+++ builtin/bins/dkron-executor-gcppubsub/gcppubsub.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+	dkplugin "github.com/distribworks/dkron/v3/plugin"
+	dktypes "github.com/distribworks/dkron/v3/plugin/types"
+)
+
+// GCPPubSub plugin publish message to topic when Execute method is called.
+type GCPPubSub struct {
+}
+
+// Execute Process method of the plugin
+// "executor": "gcppubsub",
+// "executor_config": {
+//	   "project": "project-id",
+//     "topic": "topic-name",
+//	   "data": "aGVsbG8gd29ybGQ=" // Optional. base64 encoded data
+//	   "attributes": "{\"hello\":\"world\",\"waka\":\"paka\"}" // JSON serialized attributes
+// }
+func (g *GCPPubSub) Execute(args *dktypes.ExecuteRequest, cb dkplugin.StatusHelper) (*dktypes.ExecuteResponse, error) {
+	out, err := g.ExecuteImpl(args)
+	resp := &dktypes.ExecuteResponse{Output: out}
+	if err != nil {
+		resp.Error = err.Error()
+	}
+	return resp, nil
+}
+
+// ExecuteImpl do http request
+func (g *GCPPubSub) ExecuteImpl(args *dktypes.ExecuteRequest) ([]byte, error) {
+	ctx := context.Background()
+	projectID := args.Config["project"]
+	topicName := args.Config["topic"]
+	encodedData := args.Config["data"]
+	attributesJSON := args.Config["attributes"]
+
+	if projectID == "" {
+		return nil, fmt.Errorf("missing project")
+	}
+
+	if topicName == "" {
+		return nil, fmt.Errorf("missing topic")
+	}
+
+	if attributesJSON == "" && encodedData == "" {
+		return nil, fmt.Errorf("at least one of these fields should be set 'attributes, data'")
+	}
+
+	msg := &pubsub.Message{}
+
+	var attributes map[string]string
+	if attributesJSON != "" {
+		err := json.Unmarshal([]byte(attributesJSON), &attributes)
+		if err != nil {
+			return nil, fmt.Errorf("invalid attributes JSON: %w", err)
+		}
+		msg.Attributes = attributes
+	}
+
+	if encodedData != "" {
+		data, err := base64.StdEncoding.DecodeString(encodedData)
+		if err != nil {
+			return nil, fmt.Errorf("invalid encoded data: %w", err)
+		}
+		msg.Data = data
+	}
+
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("create client for project %q: %w", projectID, err)
+	}
+	defer func() {
+		_ = client.Close()
+	}()
+
+	topic := client.Topic(topicName)
+	res := topic.Publish(ctx, msg)
+	serverID, err := res.Get(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("publish message to topic %q: %w", topicName, err)
+	}
+	return []byte(serverID), nil
+}
+
+func ConfigToPubSubMessage(config map[string]string) (*pubsub.Message, error) {
+	if config == nil {
+		return nil, fmt.Errorf("invalid config")
+	}
+
+	encodedData := config["data"]
+	attributesJSON := config["attributes"]
+
+	if attributesJSON == "" && encodedData == "" {
+		return nil, fmt.Errorf("at least one of these fields should be set 'attributes, data'")
+	}
+
+	msg := &pubsub.Message{}
+
+	var attributes map[string]string
+	if attributesJSON != "" {
+		err := json.Unmarshal([]byte(attributesJSON), &attributes)
+		if err != nil {
+			return nil, fmt.Errorf("invalid attributes JSON: %w", err)
+		}
+		msg.Attributes = attributes
+	}
+
+	if encodedData != "" {
+		data, err := base64.StdEncoding.DecodeString(encodedData)
+		if err != nil {
+			return nil, fmt.Errorf("invalid encoded data: %w", err)
+		}
+		msg.Data = data
+	}
+
+	return msg, nil
+}
---
+++ builtin/bins/dkron-executor-gcppubsub/gcppubsub_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfigToPubSubMessage(t *testing.T) {
+	tests := []struct {
+		name     string
+		config   map[string]string
+		expected *pubsub.Message
+		wantErr  bool
+	}{
+		{
+			name: "missing both data and attributes",
+			config: map[string]string{
+				"other": "key",
+			},
+			wantErr: true,
+		},
+		{
+			name:    "nil config",
+			config:  nil,
+			wantErr: true,
+		},
+		{
+			name: "only data",
+			config: map[string]string{
+				"data": "aGVsbG8gd29ybGQ=",
+			},
+			expected: &pubsub.Message{Data: []byte("hello world")},
+		},
+		{
+			name: "only attributes",
+			config: map[string]string{
+				"attributes": "{\"hello\":\"world\",\"waka\":\"paka\"}",
+			},
+			expected: &pubsub.Message{
+				Attributes: map[string]string{"hello": "world", "waka": "paka"},
+			},
+		},
+		{
+			name: "attributes and data",
+			config: map[string]string{
+				"data": "aGVsbG8gd29ybGQ=",
+				"attributes": "{\"hello\":\"world\",\"waka\":\"paka\"}",
+			},
+			expected: &pubsub.Message{
+				Data: []byte("hello world"),
+				Attributes: map[string]string{"hello": "world", "waka": "paka"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ConfigToPubSubMessage(tt.config)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, got)
+				assert.EqualValues(t, tt.expected.Attributes, got.Attributes)
+				assert.Equal(t, tt.expected.Data, got.Data)
+			}
+		})
+	}
+}
