4ee5d975211b606b473a028e7d7ec67fdb3dfe79
Yuri van Oers <yvanoers@gmail.com>
2021-05-23 18:35:00 +0200

    Add @minutely spec again

--- extcron/extparser_test.go
+++ extcron/extparser_test.go
@@ -17,6 +17,14 @@ func TestCustomSpecSchedule(t *testing.T) {
 		{"@at 2018-01-02T15:04:00Z", SimpleSchedule{time.Date(2018, time.January, 2, 15, 4, 0, 0, time.UTC)}},
 		{"@at 2019-02-04T09:20:00+06:00", SimpleSchedule{time.Date(2019, time.February, 4, 9, 20, 0, 0, time.FixedZone("", 21600))}},
 		{"@manually", SimpleSchedule{time.Time{}}},
+		{"@minutely", &cron.SpecSchedule{
+			Second:   0b1,
+			Minute:   (1 << 63) + 0b111111111111111111111111111111111111111111111111111111111111,
+			Hour:     (1 << 63) + 0b111111111111111111111111,
+			Dom:      (1 << 63) + 0b11111111111111111111111111111110,
+			Month:    (1 << 63) + 0b1111111111110,
+			Dow:      (1 << 63) + 0b1111111,
+			Location: time.Local}},
 	}
 
 	for _, c := range entries {
