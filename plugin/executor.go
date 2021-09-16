9bce6e3e7c5fd7f5c1e5fd11ba95b0beb19bdd31
Yuri van Oers <yvanoers@gmail.com>
2021-09-16 10:23:35 +0200

    Make Execute() wait for initialization of status gRPC server before use (#835)
    * Make Execute() wait for initialization of status gRPC server before use
    
    * Add test to check Execute(...) does not panic

--- plugin/executor.go
+++ plugin/executor.go
@@ -37,27 +37,42 @@ func (p *ExecutorPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBrok
 	return &ExecutorClient{client: types.NewExecutorClient(c), broker: broker}, nil
 }
 
+type Broker interface {
+	NextId() uint32
+	AcceptAndServe(id uint32, s func([]grpc.ServerOption) *grpc.Server)
+}
+
 // Here is the gRPC client that GRPCClient talks to.
 type ExecutorClient struct {
 	// This is the real implementation
 	client types.ExecutorClient
-	broker *plugin.GRPCBroker
+	broker Broker
 }
 
 func (m *ExecutorClient) Execute(args *types.ExecuteRequest, cb StatusHelper) (*types.ExecuteResponse, error) {
 	// This is where the magic conversion to Proto happens
 	statusHelperServer := &GRPCStatusHelperServer{Impl: cb}
 
+	initChan := make(chan bool, 1)
 	var s *grpc.Server
 	serverFunc := func(opts []grpc.ServerOption) *grpc.Server {
 		s = grpc.NewServer(opts...)
 		types.RegisterStatusHelperServer(s, statusHelperServer)
+		initChan <- true
 
 		return s
 	}
 
 	brokerID := m.broker.NextId()
-	go m.broker.AcceptAndServe(brokerID, serverFunc)
+	go func() {
+		m.broker.AcceptAndServe(brokerID, serverFunc)
+		// AcceptAndServe might terminate without calling serverFunc
+		// To prevent eternal blocking, send 'init done' signal
+		initChan <- true
+	}()
+
+	// Wait for s to be initialized in the goroutine
+	<-initChan
 
 	args.StatusServer = brokerID
 	r, err := m.client.Execute(context.Background(), args)
