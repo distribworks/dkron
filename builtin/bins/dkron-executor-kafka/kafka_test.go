3d91bcda464f6478de40db8d4e2d97da5e5af860
devops <devops@bestkeptproperty.com>
2021-10-10 05:47:14 +1000

    Add unit test for with/without Kafka executor message key

--- builtin/bins/dkron-executor-kafka/kafka_test.go
+++ builtin/bins/dkron-executor-kafka/kafka_test.go
@@ -7,9 +7,29 @@ import (
 	dktypes "github.com/distribworks/dkron/v3/plugin/types"
 )
 
-func TestProduceExecute(t *testing.T) {
+func TestProduceExecuteWithKey(t *testing.T) {
 	pa := &dktypes.ExecuteRequest{
-		JobName: "testJob",
+		JobName: "testJobWithKey",
+		Config: map[string]string{
+			"topic":         "test",
+			"brokerAddress": "testaddress",
+			"key":           "testkey",
+			"message":       "{\"hello\":11}",
+			"debug":         "true",
+		},
+	}
+	kafka := &Kafka{}
+	output, err := kafka.Execute(pa, nil)
+	fmt.Println(string(output.Output))
+	fmt.Println(err)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProduceExecuteWithoutKey(t *testing.T) {
+	pa := &dktypes.ExecuteRequest{
+		JobName: "testJobWithoutKey",
 		Config: map[string]string{
 			"topic":         "test",
 			"brokerAddress": "testaddress",
