4189be3bed57938b582db532718bc3adb12015e1
Filipe Pina <636320+fopina@users.noreply.github.com>
2021-11-10 11:30:08 +0000

    refresh current page after toggle/run job is used

--- ui/src/jobs/RunButton.tsx
+++ ui/src/jobs/RunButton.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNotify, useRedirect, fetchStart, fetchEnd, Button } from 'react-admin';
+import { useNotify, useRefresh, fetchStart, fetchEnd, Button } from 'react-admin';
 import { apiUrl } from '../dataProvider';
 import RunIcon from '@material-ui/icons/PlayArrow';
 
 const RunButton = ({record}: any) => {
     const dispatch = useDispatch();
-    const redirect = useRedirect();
+    const refresh = useRefresh();
     const notify = useNotify();
     const [loading, setLoading] = useState(false);
     const handleClick = () => {
@@ -16,7 +16,7 @@ const RunButton = ({record}: any) => {
         fetch(`${apiUrl}/jobs/${record.id}`, { method: 'POST' })
             .then(() => {
                 notify('Success running job');
-                redirect('/jobs');
+                refresh();
             })
             .catch((e) => {
                 notify('Error on running job', 'warning')
