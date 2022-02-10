8155faef55890d1dbe0c076077b8603275f450f2
Victor Castell <victor@victorcastell.com>
2022-02-10 08:18:42 +0100

    New site scaffold

---
+++ docus/docs/usage/processors/syslog.md
@@ -0,0 +1,31 @@
+---
+title: Syslog Processor
+---
+
+Syslog processor writes the execution output to the system syslog daemon
+
+Note: Only work on linux systems
+
+## Configuration
+
+Parameters
+
+`forward: Forward the output to the next processor`
+
+Example
+
+```json
+{
+    "name": "job_name",
+    "command": "echo 'Hello syslog'",
+    "schedule": "@every 2m",
+    "tags": {
+        "role": "web"
+    },
+    "processors": {
+        "syslog": {
+            "forward": "true"
+        }
+    }
+}
+```
---
+++ docus/docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Dkron',
+  tagline: 'Easy, Reliable Cron jobs',
+  url: 'https://dkron.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'distribworks', // Usually your GitHub org/user name.
+  projectName: 'dkron', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/dkron',
+              },
+              {
+                label: 'Gitter',
+                href: 'https://gitter.im/distribworks/dkron',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/distribworks',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/distribworks/dkron',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Distributed Works. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
