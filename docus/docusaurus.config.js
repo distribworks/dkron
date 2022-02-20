f0bba0756acd7408bd17d3fde91e677615ec39a2
Victor Castell <victor@victorcastell.com>
2022-02-21 00:01:19 +0100

    details

--- docus/docusaurus.config.js
+++ docus/docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Intro',
                 to: '/docs/intro',
               },
             ],
