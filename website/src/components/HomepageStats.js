58ce019ce6a3d5eb575eb41b1cca84fb961a8613
Victor Castell <victor@victorcastell.com>
2022-04-19 23:29:34 +0200

    UI

--- website/src/components/HomepageStats.js
+++ website/src/components/HomepageStats.js
@@ -7,13 +7,13 @@ export default function HomepageStats() {
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <h2 className="text-4xl font-bold text-fuchsia-700">Stats</h2>
           <div className="flex flex-row">
-            <div class="basis-1/2">
-              <h3>Clusters Running</h3>
+            <div class="basis-1/2 ">
               <CountUp end={807} duration={5} useEasing={true} className="text-6xl font-bold" />
+              <h3>Clusters Running</h3>
             </div>
             <div class="basis-1/2">
-              <h3>Servers Running</h3>
               <CountUp end={2536} duration={5} useEasing={true} className="text-6xl font-bold" />
+              <h3>Servers Running</h3>
             </div>
           </div>
         </div>
