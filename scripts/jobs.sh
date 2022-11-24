a9d9378e02489db73bb392afe2776b41e367290e
Victor Castell <victor@victorcastell.com>
2022-11-24 09:08:14 +0100

    Test jobs

--- scripts/jobs.sh
+++ scripts/jobs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-for i in {1..10000}
+for i in {1..2000}
 do
-   curl localhost:8080/v1/jobs -d "{
+   curl test.dkron.io:8080/v1/jobs -d "{
     \"name\": \"test_job_$i\",
-    \"schedule\": \"@every $(($RANDOM%60+1))s\",
-	\"executor\": \"shell\",
+    \"schedule\": \"@every $(($RANDOM%10+1))s\",
+	\"concurrency\": \"forbid\",
+	\"tags\": {
+            \"role\": \"dkron:1\"
+          },
+	\"executor\": \"http\",
   	\"executor_config\": {
-    	\"command\": \"echo 'run job $i'\"
+            \"method\": \"GET\",
+            \"url\": \"https://httpbin.org/get\"
+          }
   	},
-	\"ephemeral\": true
+	\"ephemeral\": false,
 }"
 done
