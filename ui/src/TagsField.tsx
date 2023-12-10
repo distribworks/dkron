7ae0bfd3ca239c59367615cb565887b737011b15
Victor Castell <victor@victorcastell.com>
2023-12-10 21:59:51 +0100

    Checks

--- ui/src/TagsField.tsx
+++ ui/src/TagsField.tsx
@@ -1,12 +1,16 @@
 import { Chip } from '@mui/material';
 
-export const TagsField = ({ record }: any) => (
-    <ul>
-        {Object.keys(record.Tags).map(key => (
-            <Chip label={key+": "+record.Tags[key]} />
-        ))}
-    </ul>
-)
+export const TagsField = ({ record }: any) => {
+    if (record === undefined) {
+		return null
+    } else {
+        return <ul>
+            {Object.keys(record.Tags).map(key => (
+                <Chip label={key+": "+record.Tags[key]} />
+            ))}
+        </ul>
+    }
+};
 TagsField.defaultProps = {
     addLabel: true
 };
