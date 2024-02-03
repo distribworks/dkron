6bf227c3890d6959031cccdf7968fafb83803da1
Victor Castell <0x@vcastellm.xyz>
2024-02-03 18:27:04 +0100

    Fix cards with icons

--- ui/src/dashboard/CardWithIcon.tsx
+++ ui/src/dashboard/CardWithIcon.tsx
@@ -1,69 +1,71 @@
 import { FC, createElement } from 'react';
+import { styled } from '@mui/material/styles';
 import { Card, Box, Typography, Divider } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { Link } from 'react-router-dom';
-
-import cartouche from './cartouche.png';
+import { Link, To } from 'react-router-dom';
+import { ReactNode } from 'react';
 
 interface Props {
     icon: FC<any>;
-    to: string;
+    to: To;
     title?: string;
-    subtitle?: string | number;
+    subtitle?: ReactNode;
+    children?: ReactNode;
 }
 
-const useStyles = makeStyles(theme => ({
-    card: {
-        minHeight: 52,
-        display: 'flex',
-        flexDirection: 'column',
-        flex: '1',
-        '& a': {
-            textDecoration: 'none',
-            color: 'inherit',
-        },
-    },
-    main: (props: Props) => ({
-        overflow: 'inherit',
-        padding: 16,
-        background: `url(${ cartouche }) no-repeat`,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        '& .icon': {
-            color: '#dc2440',
-        },
-    }),
-    title: {},
-}));
-
-const CardWithIcon: FC<Props> = props => {
-    const { icon, title, subtitle, to, children } = props;
-    const classes = useStyles(props);
-    return (
-        <Card className={classes.card}>
-            <Link to={to}>
-                <div className={classes.main}>
-                    <Box width="3em" className="icon">
-                        {createElement(icon, { fontSize: 'large' })}
-                    </Box>
-                    <Box textAlign="right">
-                        <Typography
-                            className={classes.title}
-                            color="textSecondary"
-                        >
-                            {title}
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            {subtitle || ' '}
-                        </Typography>
-                    </Box>
-                </div>
-            </Link>
-            {children && <Divider />}
-            {children}
-        </Card>
-    );
-};
+const CardWithIcon = ({ icon, title, subtitle, to, children }: Props) => (
+    <Card
+        sx={{
+            minHeight: 52,
+            display: 'flex',
+            flexDirection: 'column',
+            flex: '1',
+            '& a': {
+                textDecoration: 'none',
+                color: 'inherit',
+            },
+        }}
+    >
+        <Link to={to}>
+            <Box
+                sx={{
+                    position: 'relative',
+                    overflow: 'hidden',
+                    padding: '16px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    '& .icon': {
+                        color: 'secondary.main',
+                    },
+                    '&:before': {
+                        position: 'absolute',
+                        top: '50%',
+                        left: 0,
+                        display: 'block',
+                        content: `''`,
+                        height: '200%',
+                        aspectRatio: '1',
+                        transform: 'translate(-30%, -60%)',
+                        borderRadius: '50%',
+                        backgroundColor: 'secondary.main',
+                        opacity: 0.15,
+                    },
+                }}
+            >
+                <Box width="3em" className="icon">
+                    {createElement(icon, { fontSize: 'large' })}
+                </Box>
+                <Box textAlign="right">
+                    <Typography color="textSecondary">{title}</Typography>
+                    <Typography variant="h5" component="h2">
+                        {subtitle || ' '}
+                    </Typography>
+                </Box>
+            </Box>
+        </Link>
+        {children && <Divider />}
+        {children}
+    </Card>
+);
 
 export default CardWithIcon;
