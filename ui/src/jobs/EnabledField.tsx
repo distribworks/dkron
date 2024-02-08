a1e1799900c10c034ef2be2ccfde25afee79946a
Victor Castell <0x@vcastellm.xyz>
2024-02-08 23:13:18 +0100

    Adapt fields to admin v4

--- ui/src/jobs/EnabledField.tsx
+++ ui/src/jobs/EnabledField.tsx
@@ -1,9 +1,12 @@
 import SuccessIcon from '@mui/icons-material/CheckCircle';
 import FailedIcon from '@mui/icons-material/Cancel';
 import { Tooltip } from '@mui/material';
+import { useRecordContext } from 'react-admin';
 
-const EnabledField = (props: any) => {
-    if (props.record !== undefined && props.record[props.source] === true) {
+const EnabledField = () => {
+    const record = useRecordContext();
+
+    if (record.disabled) {
         return <Tooltip title="Disabled"><FailedIcon htmlColor="red" /></Tooltip>
     } else {
         return <Tooltip title="Enabled"><SuccessIcon htmlColor="green" /></Tooltip>
--- ui/src/jobs/StatusField.tsx
+++ ui/src/jobs/StatusField.tsx
@@ -2,18 +2,17 @@ import SuccessIcon from '@mui/icons-material/CheckCircle';
 import FailedIcon from '@mui/icons-material/Cancel';
 import UntriggeredIcon from '@mui/icons-material/Timer';
 import { Tooltip } from '@mui/material';
+import { useRecordContext } from 'react-admin';
 
-const StatusField = (props: any) => {
-	if (props.record === undefined) {
-		return null
+const StatusField = () => {
+	const record = useRecordContext();
+	
+	if (record.status === 'success') {
+		return <Tooltip title="Success"><SuccessIcon htmlColor="green" /></Tooltip> 
+	} else if (record.status === 'failed') {
+		return <Tooltip title="Error"><FailedIcon htmlColor="red" /></Tooltip>
 	} else {
-		if (props.record[props.source] === 'success') {
-			return <Tooltip title="Success"><SuccessIcon htmlColor="green" /></Tooltip> 
-		} else if (props.record[props.source] === 'failed') {
-			return <Tooltip title="Error"><FailedIcon htmlColor="red" /></Tooltip>
-		} else {
-			return <Tooltip title="Waiting to Run"><UntriggeredIcon htmlColor="blue" /></Tooltip>
-		}
+		return <Tooltip title="Waiting to Run"><UntriggeredIcon htmlColor="blue" /></Tooltip>
 	}
 };
 
