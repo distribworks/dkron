e547e6186605e53f6e359d66cd8acd3c9d082d0b
Victor Castell <0x@vcastellm.xyz>
2024-10-06 13:14:51 +0200

    fix: use httpClient in references

--- ui/src/dataProvider.ts
+++ ui/src/dataProvider.ts
@@ -36,7 +36,7 @@ const dkronDataProvider: DataProvider = {
         };
         const url = `${apiUrl}/${params.target}/${params.id}/${resource}?${stringify(query)}`;
 
-        return fetchUtils.fetchJson(url).then(({ headers, json }) => {
+        return httpClient(url).then(({ headers, json }) => {
             if (!headers.has('x-total-count')) {
                 throw new Error(
                     'The X-Total-Count header is missing in the HTTP Response. The jsonServer Data Provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare X-Total-Count in the Access-Control-Expose-Headers header?'
