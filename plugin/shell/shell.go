d8e419fc7d36f307c5e817467672d97f5ab7e652
Victor Castell <0x@vcastellm.xyz>
2025-09-23 17:52:19 +0200

    fix: Comment out process metrics collection code (#1813)

--- plugin/shell/shell.go
+++ plugin/shell/shell.go
@@ -130,14 +130,14 @@ func (s *Shell) ExecuteImpl(args *dktypes.ExecuteRequest, cb dkplugin.StatusHelp
 		defer slowTimer.Stop()
 	}
 
-	quit := make(chan int)
-
 	// FIXME: Debug metrics collection
+	// quit := make(chan int)
+
 	// go CollectProcessMetrics(args.JobName, cmd.Process.Pid, quit)
 
-	err = cmd.Wait()
-	quit <- cmd.ProcessState.ExitCode()
-	close(quit) // exit metric refresh goroutine after job is finished
+	// err = cmd.Wait()
+	// quit <- cmd.ProcessState.ExitCode()
+	// close(quit) // exit metric refresh goroutine after job is finished
 
 	if jobTimedOut {
 		_, err := output.Write([]byte(jobTimeoutMessage))
